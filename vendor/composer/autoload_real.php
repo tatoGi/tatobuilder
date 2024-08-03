6ddcc79e06d9ef7b265545c130aab8a2afdcfb81
tatoGi <workspace.tato@gmail.com>
2024-08-03 22:38:33 +0400

    fix

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit24a93ca1aba546459a7053246991ca8c
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit24a93ca1aba546459a7053246991ca8c', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit24a93ca1aba546459a7053246991ca8c', 'loadClassLoader'));
@@ -32,18 +30,6 @@ class ComposerAutoloaderInit24a93ca1aba546459a7053246991ca8c
         call_user_func(\Composer\Autoload\ComposerStaticInit24a93ca1aba546459a7053246991ca8c::getInitializer($loader));
 
         $loader->register(true);
-
-        $filesToLoad = \Composer\Autoload\ComposerStaticInit24a93ca1aba546459a7053246991ca8c::$files;
-        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-                require $file;
-            }
-        }, null, null);
-        foreach ($filesToLoad as $fileIdentifier => $file) {
-            $requireFile($fileIdentifier, $file);
-        }
 
         return $loader;
     }
