537c43c1333096a7b81067d664c57abe5c5a7cb4
tatoGi <workspace.tato@gmail.com>
2024-08-03 23:26:44 +0400

    fix

---
+++ src/database/migrations/2022_10_25_143620_create_form_fields_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('form_fields', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->integer('type')->default(1);
+            $table->json('data')->nullable();
+            $table->json('validation')->nullable();
+            $table->unsignedBigInteger('form_id');
+            $table->timestamps();
+
+            $table->foreign('form_id')->references('id')->on('forms');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('form_fields');
+    }
+};
