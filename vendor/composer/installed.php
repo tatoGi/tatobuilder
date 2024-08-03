6d0f61fdc326a6aac5aad93e7204d338e3fd6095
tatoGi <workspace.tato@gmail.com>
2024-08-03 23:40:59 +0400

    fix

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'tatobuilder/formbuilder',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '537c43c1333096a7b81067d664c57abe5c5a7cb4',
+        'reference' => 'ff39d10c1f1a638fed6ae7bd5b21a19bf2103c89',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'tatobuilder/formbuilder' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '537c43c1333096a7b81067d664c57abe5c5a7cb4',
+            'reference' => 'ff39d10c1f1a638fed6ae7bd5b21a19bf2103c89',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
